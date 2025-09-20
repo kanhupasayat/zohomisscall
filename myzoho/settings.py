2172ea710ca1b53ff543235784523c9af345e00e
kanhupasayat <111569616+kanhupasayat@users.noreply.github.com>
2025-09-20 19:17:55 +0530

    Update settings.py
    url change

--- myzoho/settings.py
+++ myzoho/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n(9b@ue85(megd15@x*d4)mvbt(vzf#(j-08=7=zu52p3$@#9x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["zohodashbord.onrender.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["zohomisscalls.onrender.com", "127.0.0.1", "localhost"]
 
 
 CORS_ALLOW_ALL_ORIGINS = True
