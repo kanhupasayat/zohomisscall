a0c152d3ff85a982285859283d09bad8b32cedce
kanhupasayat <111569616+kanhupasayat@users.noreply.github.com>
2025-09-20 19:14:36 +0530

    Update settings.py
    ok

--- myzoho/settings.py
+++ myzoho/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,6 +88,18 @@ CACHES = {
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+STATIC_URL = '/static/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
 
 DATABASES = {
     'default': {
